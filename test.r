c2628fe79746a6685f7926ac4b0f8d11bdd52c31
syanification <syanification@gmail.com>
2025-01-02 14:16:23 -0800

    Added function to get every minor leaguer from the last decade

---
+++ test.r
@@ -0,0 +1,12 @@
+library(baseballr)
+library(dplyr)
+source("getMinors.R")
+
+# data <- bref_daily_batter("2015-08-01", "2015-10-03") 
+# data %>%
+#   dplyr::glimpse()
+
+data <- getMinors() 
+data %>%
+  dplyr::glimpse()
+
