0fad0b2e470bf223276f1a9747eaf50ce1613dd3
syanification <syanification@gmail.com>
2025-01-17 09:26:00 -0500

    Added file to prep data for ML

--- cleanup.R
+++ cleanup.R
@@ -1,4 +1,8 @@
 library(dplyr)
 library(tidyr)
 
-data <- read.csv("Data/commonData.csv", header = TRUE)
+data <- read.csv("Data/common2.csv", header = TRUE)
+
+data <- data %>% select(BA.x, BA.y, OBP.x, OBP.y, PA.x, PA.y)
+
+write.csv(data, "Data/common_cleaned.csv", row.names = FALSE)
