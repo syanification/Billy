754fe1128b41aea6b8b62dfb60ed755608fd786e
syanification <syanification@gmail.com>
2025-01-17 09:28:31 -0500

    WE HAVE SLG AND OPS BABY WOOOOO

--- cleanup.R
+++ cleanup.R
@@ -3,6 +3,6 @@ library(tidyr)
 
 data <- read.csv("Data/common2.csv", header = TRUE)
 
-data <- data %>% select(BA.x, BA.y, OBP.x, OBP.y, PA.x, PA.y)
+data <- data %>% select(BA.x, BA.y, OBP.x, OBP.y, SLG.x, SLG.y, OPS.x, OPS.y, PA.x, PA.y)
 
 write.csv(data, "Data/common_cleaned.csv", row.names = FALSE)
--- combine.R
+++ combine.R
@@ -12,7 +12,7 @@ minor_data_short <- minor_data_short %>% distinct(Name, .keep_all = TRUE)
 common_data <- inner_join(minor_data_short, major_data, by = "Name")
 
 # Display the resulting data
-common_data <- common_data %>% select(Name, BA.x, BA.y, OBP.x, OBP.y, PA.x, PA.y)
+common_data <- common_data %>% select(Name, BA.x, BA.y, OBP.x, OBP.y, SLG.x, SLG.y, OPS.x, OPS.y, PA.x, PA.y)
 common_data <- common_data %>% drop_na()
 common_data %>% glimpse()
 
