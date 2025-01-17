2a82b3ede693ac870a4265ec742813bdcbebf365
syanification <syanification@gmail.com>
2025-01-16 20:54:42 -0500

    Attempting to fix data problems

--- test.r
+++ test.r
@@ -5,40 +5,41 @@ source("getMinors.R")
 source("getMajors.R")
 source("minorDaily.R")
 
-print("Majors:")
+# print("Majors:")
 
-majorData <- getMajors(3650) %>% distinct(Name, Age, .keep_all = TRUE)
-majorData %>% dplyr::glimpse()
+# majorData <- getMajors(3650) %>% distinct(Name, Age, .keep_all = TRUE)
+# majorData %>% dplyr::glimpse()
 
 print("Minors:")
 
-minorData1 <- minorDaily("2007-01-01","2016-01-01") %>% distinct(Name, Age, .keep_all = TRUE)
-minorData2 <- minorDaily("2016-01-01","2025-01-01") %>% distinct(Name, Age, .keep_all = TRUE)
+# minorData1 <- minorDaily("2007-01-01","2016-01-01") %>% distinct(Name, Age, .keep_all = TRUE)
+# minorData2 <- minorDaily("2016-01-01","2025-01-01") %>% distinct(Name, Age, .keep_all = TRUE)
 
-# Full join minorData1 and minorData2
-minorData <- full_join(minorData1, minorData2, by = c("Name", "Age"))
+# # Full join minorData1 and minorData2
+# minorData <- full_join(minorData1, minorData2, by = c("Name", "Age"))
 
-# Remove duplicates
-minorData <- minorData %>% distinct(Name, Age, .keep_all = TRUE)
+# # Remove duplicates
+# minorData <- minorData %>% distinct(Name, Age, .keep_all = TRUE)
 
-# Display the resulting data
-minorData %>% glimpse()
-
+# # Display the resulting data
+# minorData %>% glimpse()
 
 # minorData1 %>% dplyr::glimpse()
 
-# minorData <- getMinors(3650) %>% distinct(Name, Age, .keep_all = TRUE)
-# minorData %>% dplyr::glimpse()
+minorData <- getMinors(3650) %>% distinct(Name, Age, .keep_all = TRUE)
+minorData %>% dplyr::glimpse()
 
 # Perform an inner join on the Name and Age columns
-commonData <- inner_join(minorData, majorData, by = "Name")
+# commonData <- inner_join(minorData, majorData, by = "Name")
 
-# Display the resulting data
-commonData <- commonData %>% select(Name, BA.x, BA.y, PA.x, PA.y, OBP.x, OBP.y)
-commonData <- commonData %>% drop_na()
-commonData %>% glimpse()
+# # Display the resulting data
+# commonData <- commonData %>% select(Name, BA.x, BA.y, PA.x, PA.y, OBP.x, OBP.y)
+# commonData <- commonData %>% drop_na()
+# commonData %>% glimpse()
 
-write.csv(commonData, "commonData.csv", row.names = FALSE)
+# write.csv(commonData, "Data/commonData.csv", row.names = FALSE)
+# write.csv(majorData, "Data/majorData.csv", row.names = FALSE)
+write.csv(minorData, "Data/minorShortData.csv", row.names = FALSE)
 
 # write.csv(majorData, "majorData.csv", row.names = FALSE)
 
