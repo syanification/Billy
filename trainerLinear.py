88942da324b80391b148e924b00963eb165e86c7
syanification <syanification@gmail.com>
2025-02-01 17:45:28 -0500

    Zoomed in on early epochs

--- trainerLinear.py
+++ trainerLinear.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     #   of hyperparameters to find optimal
     results = []
 
-    for e in range(10, 110, 10):
+    for e in range(1, 50):
         for a in alphas:
             finalModel, testLoss, testMae = train('Data/commonCleaned.csv', e, numFolds, a)
             # Append the results to the list
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     resultsDF = pd.DataFrame(results)
 
     # Save the DataFrame to a CSV file with an index
-    resultsDF.to_csv('Training Evaluation/trainingResultsLinear.csv', index=False)
+    resultsDF.to_csv('Training Evaluation/trainingResultsLinearMicro.csv', index=False)
 
 
     # trainedModel = train('Data/commonCleaned.csv', epochs, numFolds, alpha)
